6960728baa8c0cd3f6d285675d227731dc59ec1e
Jakub Kulig <info@jkulig.com>
2022-08-01 18:41:26 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    base: '/interactive-rating-component/'
-  }
+  base: '/interactive-rating-component/',
+  plugins: [svelte()]
 })
